7636e5784cc897a47c2d94d7dab6868ffe52a11c
IKala4I <vpersko@gmail.com>
2023-11-22 13:58:22 +0200

    + draft filtering by tags is created

--- src/app/products/mock-data/products.ts
+++ src/app/products/mock-data/products.ts
@@ -1,9 +1,10 @@
 import {IProduct, Product} from '../product.model'
+import TAGS from './tags'
 
-const PRODUCTS:IProduct[] = [
+const PRODUCTS: IProduct[] = [
   new Product('meal', 150, undefined, 'cool meal'),
-  new Product('apple', 30, [{id: 1, name: 'fruit'}], 'tasty apple'),
-  new Product('soup', 85, [{id: 2, name: 'first-dish'}, {id: 3, name: 'veggie'}], 'yummy soup'),
+  new Product('apple', 30, [TAGS[0]], 'tasty apple'),
+  new Product('soup', 85, [TAGS[1], TAGS[2]], 'yummy soup'),
 ]
 
 export default PRODUCTS
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {Component, OnInit} from '@angular/core'
+import {CommonModule} from '@angular/common'
 import {Router} from '@angular/router'
 import {IProduct, ITag} from '../product.model'
 import PRODUCTS from '../mock-data/products'
@@ -13,10 +13,13 @@ import TAGS from '../mock-data/tags'
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent implements OnInit {
-  private products!: IProduct[];
-  tags!: ITag[];
+  private products!: IProduct[]
+  tags!: ITag[]
 
-  filteredProducts!: IProduct[] | null;
+  filteredProducts?: IProduct[]
+  selectedTags!: string[]
+
+  isNoTags: boolean = false
 
   constructor(private router: Router) {
   }
@@ -25,23 +28,76 @@ export class ProductListComponent implements OnInit {
     this.products = PRODUCTS
     this.tags = TAGS
     this.filteredProducts = this.products
+    this.selectedTags = []
   }
 
   public showDetail(id: string) {
     this.router.navigate(['detail', id])
   }
 
-  public filterProducts(tag: string) {
-    if (tag === 'all')
-      this.filteredProducts = this.products
-    else
-      this.filteredProducts = this.products.filter(product => {
-        if (product.tags) {
-          const names: string[] = product.tags.map(tag => tag.name)
-          if (names.includes(tag))
-            return product
-        }
-        return
-      })
+  public onCheckChange(event: Event) {
+    const input = event.target as HTMLInputElement
+
+    if (input.value === 'noTags') {
+      if (input.checked) {
+        this.isNoTags = true
+        this.filteredProducts = this.products.filter(product => !product.tags)
+      } else {
+        this.isNoTags = false
+        if (this.selectedTags.length) {
+          this.filteredProducts = this.products.filter(product => {
+            if (product.tags) {
+              const names: string[] = product.tags.map(tag => tag.name)
+              let isContainAllTags = true
+              this.selectedTags.every(tag => {
+                if (!names.includes(tag)) {
+                  isContainAllTags = false
+                  return false
+                }
+                return true
+              })
+              if (isContainAllTags)
+                return product
+            }
+            return
+          })
+        } else
+          this.filteredProducts = this.products
+      }
+    } else {
+      if (input.checked) {
+        this.filteredProducts = this.filteredProducts?.filter(product => {
+          if (product.tags) {
+            const names: string[] = product.tags.map(tag => tag.name)
+            if (names.includes(input.value))
+              return product
+          }
+          return
+        })
+        this.selectedTags.push(input.value)
+      } else {
+        this.selectedTags = this.selectedTags.filter(tag => tag !== input.value)
+
+        if (this.selectedTags.length) {
+          this.filteredProducts = this.products.filter(product => {
+            if (product.tags) {
+              const names: string[] = product.tags.map(tag => tag.name)
+              let isContainAllTags = true
+              this.selectedTags.every(tag => {
+                if (!names.includes(tag)) {
+                  isContainAllTags = false
+                  return false
+                }
+                return true
+              })
+              if (isContainAllTags)
+                return product
+            }
+            return
+          })
+        } else
+          this.filteredProducts = this.products
+      }
+    }
   }
 }
